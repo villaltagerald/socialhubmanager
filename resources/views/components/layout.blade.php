f138b5fded072a381f1db02a652dfb58138bd34f
Gerald Villalta Rojas <villaltageral@gmail.com>
2023-11-13 11:26:18 -0600

    Modificaciones en nav y header

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,43 +8,57 @@
 
 <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
-        <nav class="md:flex md:justify-between md:items-center">
-            <div class="flex items-center">
-                <a href="/">
-                    <img src="/images/logo.png" alt="SocialHubManager Logo" width="70" height="70">
-                    <span class="ml-2">Social Hub Manager</span>
-                </a>
-            </div>
+        
+        <div class="mb-8 flex items-baseline justify-center">
+            <button id="menu-toggle" class="text-white focus:outline-none">
+                <img src="/images/menu.svg" alt="Icono del menú" class="w-6 h-6">
+            </button>
+            @include('dashboard._header')
+        </div>
+        
+        
 
-            <div class="mt-8 md:mt-0 flex items-center">
-                @auth
-                    <x-dropdown>
-                        <x-slot name="trigger">
-                            <button class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }} !</button>
-                        </x-slot>
-
-                        <x-dropdown-item href="#" :active="request()->is('admin/posts')">Dashboard</x-dropdown-item>
-                        <x-dropdown-item href="#" :active="request()->is('admin/posts/create')">Instantaneous publication</x-dropdown-item>
-                        <x-dropdown-item href="#" :active="request()->is('admin/posts/create')">Queued publication</x-dropdown-item>
-                        <x-dropdown-item href="#" :active="request()->is('admin/posts/create')">Scheduled publishing</x-dropdown-item>
-                        
-                        <x-dropdown-item href="#" x-data="{}" :active="false" @click.prevent="document.querySelector('#logout-form').submit()">Log Out</x-dropdown-item>
-
-                        <form id="logout-form" method="POST" action="/logout" class="text-xs font-semibold text-blue-500 ml-6">
-                            @csrf
-                        </form>
-
-                    </x-dropdown>
-                    
-                    
-                @else
-                    <a href="/register" class="text-xs font-bold uppercase">Register</a>
-                    <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
-                @endauth
-
+        <nav id="nav" class=" h-screen w-1/6 bg-gray-800 text-white p-4 fixed left-0 top-0 overflow-y-auto transform -translate-x-full transition-transform duration-300 ease-in-out">
+            <div class="flex flex-col items-center justify-start h-full">
+                <div class="mb-8">
+                    <img src="/images/logo.png" alt="SocialHubManager Logo" class="w-16 h-16 mb-2">
+                </div>
+        
+                <div>
+                    @auth
+                        <div>
+                            <button class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}!</button>
+                        </div>
+                        <div class="mt-4">
+                            <x-dropdown-item href="#" :active="request()->is('admin/posts')">Dashboard</x-dropdown-item>
+                            <x-dropdown-item href="#" :active="request()->is('admin/posts/create')">Instantaneous</x-dropdown-item>
+                            <x-dropdown-item href="#" :active="request()->is('admin/posts/create')">Queued</x-dropdown-item>
+                            <x-dropdown-item href="#" :active="request()->is('admin/posts/create')">Scheduled</x-dropdown-item>
+                        </div>
+                        <div class="mt-4">
+                            <x-dropdown-item href="#" x-data="{}" :active="false" @click.prevent="document.querySelector('#logout-form').submit()">Log Out</x-dropdown-item>
+                        </div>
+                        <div class="mt-4">
+                            <form id="logout-form" method="POST" action="/logout" class="text-xs font-semibold text-blue-500">
+                                @csrf
+                            </form>
+                        </div>
+                    @else
+                        <div class="mt-4">
+                            <a href="/register" class="text-xs font-bold uppercase">Register</a>
+                        </div>
+                        <div class="mt-4">
+                            <a href="/login" class="text-xs font-bold uppercase">Log In</a>
+                        </div>
+                    @endauth
+                </div>
             </div>
         </nav>
-        {{ $slot }}
+
+        <div class="ml-1/6">
+            {{ $slot }}
+        </div>
+        
         <footer id="newsletter" class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
             <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
             <h5 class="text-3xl">Stay in touch with the latest posts</h5>
@@ -52,4 +66,23 @@
         </footer>
     </section>
     <x-flash/>
+
+    <script>
+        document.getElementById("menu-toggle").addEventListener("click", function (event) {
+            event.stopPropagation(); // Evita que el clic se propague al documento
+            const nav = document.getElementById("nav");
+            nav.classList.toggle("-translate-x-full");
+        });
+    
+        document.addEventListener("click", function (event) {
+        const nav = document.getElementById("nav");
+        const menuToggle = document.getElementById("menu-toggle");
+
+        if (event.target !== menuToggle) {
+            nav.classList.add("-translate-x-full");
+        }
+    });
+    </script>
+    
+    
 </body>
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,6 +1,20 @@
-<x-layout> 
-    @include ('dashboard._header')
-        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-            <p class="text-center">No posts yet. Please check back later. </p>
-        </main>
+<x-layout>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6 ml-1/6">
+        <div class="p-6">
+            <div class="bg-white rounded-lg shadow-lg p-6">
+                <h1 class="text-2xl font-semibold">Panel de Control</h1>
+                <p>Bienvenido al panel de control de tu aplicación.</p>
+
+                <div class="mt-4">
+                    <div class="p-4 bg-blue-200 rounded-lg">
+                        <h2 class="text-lg font-semibold">Estadísticas</h2>
+                        <p>Usuarios registrados: 100</p>
+                        <p>Publicaciones: 500</p>
+                        <p>Comentarios: 1000</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
 </x-layout>
